4dd353cb3d74e72d4995abd3339ba851b13888e7
kcl <kcl@outlook.com>
2022-10-04 04:30:04 +0800

    修改窗口标题

--- HotspotManager/Form1.Designer.cs
+++ HotspotManager/Form1.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "Form1";
+            this.Text = "HotspotManager";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
