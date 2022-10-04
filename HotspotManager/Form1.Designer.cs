a00abea984c2dc31fbaface30f84474822745678
kcl <kcl@outlook.com>
2022-10-04 20:17:26 +0800

    调整控件名称，调整窗口大小缩放，添加选项卡。

--- HotspotManager/Form1.Designer.cs
+++ HotspotManager/Form1.Designer.cs
@@ -29,76 +29,109 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.button1 = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.timer2 = new System.Windows.Forms.Timer(this.components);
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.hotspotTabPage = new System.Windows.Forms.TabPage();
+            this.hotspotButtonConnect = new System.Windows.Forms.Button();
+            this.hotspotCheckBoxAutoReconnect = new System.Windows.Forms.CheckBox();
+            this.networkTabPage = new System.Windows.Forms.TabPage();
+            this.networkCheckBoxAutoReconnect = new System.Windows.Forms.CheckBox();
+            this.hotspotTabPage.SuspendLayout();
+            this.networkTabPage.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(16, 16);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(120, 80);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "开启热点";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // timer1
             // 
             this.timer1.Interval = 5000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(20, 112);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(112, 24);
-            this.checkBox1.TabIndex = 1;
-            this.checkBox1.Text = "保持热点开启";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
-            // 
             // timer2
             // 
             this.timer2.Interval = 12000;
             this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
             // 
-            // checkBox2
+            // tabControl
             // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(20, 152);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(112, 24);
-            this.checkBox2.TabIndex = 2;
-            this.checkBox2.Text = "自动重新拨号";
-            this.checkBox2.UseVisualStyleBackColor = true;
-            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            this.tabControl.Controls.Add(this.hotspotTabPage);
+            this.tabControl.Controls.Add(this.networkTabPage);
+            this.tabControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.TabIndex = 0;
+            // 
+            // hotspotTabPage
+            // 
+            this.hotspotTabPage.Controls.Add(this.hotspotButtonConnect);
+            this.hotspotTabPage.Controls.Add(this.hotspotCheckBoxAutoReconnect);
+            this.hotspotTabPage.Name = "hotspotTabPage";
+            this.hotspotTabPage.TabIndex = 1;
+            this.hotspotTabPage.Text = "热点";
+            this.hotspotTabPage.UseVisualStyleBackColor = true;
+            // 
+            // hotspotButtonConnect
+            // 
+            this.hotspotButtonConnect.Location = new System.Drawing.Point(16, 16);
+            this.hotspotButtonConnect.Name = "hotspotButtonConnect0";
+            this.hotspotButtonConnect.Size = new System.Drawing.Size(96, 64);
+            this.hotspotButtonConnect.TabIndex = 10;
+            this.hotspotButtonConnect.Text = "开启热点";
+            this.hotspotButtonConnect.UseVisualStyleBackColor = true;
+            this.hotspotButtonConnect.Click += new System.EventHandler(this.hotspotButtonConnect_Click);
+            // 
+            // hotspotCheckBoxAutoReconnect
+            // 
+            this.hotspotCheckBoxAutoReconnect.Location = new System.Drawing.Point(16, 96);
+            this.hotspotCheckBoxAutoReconnect.Name = "hotspotCheckBoxAutoReconnect";
+            this.hotspotCheckBoxAutoReconnect.Size = new System.Drawing.Size(120, 24);
+            this.hotspotCheckBoxAutoReconnect.TabIndex = 11;
+            this.hotspotCheckBoxAutoReconnect.Text = "保持热点开启";
+            this.hotspotCheckBoxAutoReconnect.UseVisualStyleBackColor = true;
+            this.hotspotCheckBoxAutoReconnect.CheckedChanged += new System.EventHandler(this.hotspotCheckBoxAutoReconnect_CheckedChanged);
+            // 
+            // networkTabPage
+            // 
+            this.networkTabPage.Controls.Add(this.networkCheckBoxAutoReconnect);
+            this.networkTabPage.Name = "hotspotTabPage";
+            this.networkTabPage.TabIndex = 2;
+            this.networkTabPage.Text = "校园网";
+            this.networkTabPage.UseVisualStyleBackColor = true;
+            // 
+            // networkCheckBoxAutoReconnect
+            // 
+            this.networkCheckBoxAutoReconnect.Location = new System.Drawing.Point(16, 16);
+            this.networkCheckBoxAutoReconnect.Name = "networkCheckBoxAutoReconnect";
+            this.networkCheckBoxAutoReconnect.Size = new System.Drawing.Size(120, 24);
+            this.networkCheckBoxAutoReconnect.TabIndex = 20;
+            this.networkCheckBoxAutoReconnect.Text = "自动重新拨号";
+            this.networkCheckBoxAutoReconnect.UseVisualStyleBackColor = true;
+            this.networkCheckBoxAutoReconnect.CheckedChanged += new System.EventHandler(this.networkCheckBoxAutoReconnect_CheckedChanged);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.checkBox2);
-            this.Controls.Add(this.checkBox1);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(360, 480);
+            this.Controls.Add(this.tabControl);
             this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(8, 8, 8, 8);
             this.Text = "HotspotManager";
+            this.tabControl.ResumeLayout(false);
+            this.hotspotTabPage.ResumeLayout(false);
+            this.hotspotTabPage.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private Button button1;
         private System.Windows.Forms.Timer timer1;
-        private CheckBox checkBox1;
         private System.Windows.Forms.Timer timer2;
-        private CheckBox checkBox2;
+        private TabControl tabControl;
+        private TabPage hotspotTabPage;
+        private Button hotspotButtonConnect;
+        private CheckBox hotspotCheckBoxAutoReconnect;
+        private TabPage networkTabPage;
+        private CheckBox networkCheckBoxAutoReconnect;
     }
 }
--- HotspotManager/Form1.cs
+++ HotspotManager/Form1.cs
@@ -12,51 +12,51 @@ namespace HotspotManager
             InitializeComponent();
         }
 
-        private async void button1_Click(object sender, EventArgs e)
+        private async void hotspotButtonConnect_Click(object sender, EventArgs e)
         {
             var connectionProfile = NetworkInformation.GetInternetConnectionProfile();
             var tetheringManager = NetworkOperatorTetheringManager.CreateFromConnectionProfile(connectionProfile);
-            if (button1.Text == "开启热点")
+            if (hotspotButtonConnect.Text == "开启热点")
             {
                 switch (tetheringManager.TetheringOperationalState)
                 {
                     case TetheringOperationalState.On:
-                        button1.Text = "关闭热点";
+                        hotspotButtonConnect.Text = "关闭热点";
                         break;
                     case TetheringOperationalState.Off:
-                        button1.Text = "正在等待";
+                        hotspotButtonConnect.Text = "正在等待";
                         var result = await tetheringManager.StartTetheringAsync();
                         if (result.Status == TetheringOperationStatus.Success)
-                            button1.Text = "关闭热点";
+                            hotspotButtonConnect.Text = "关闭热点";
                         break;
                     case TetheringOperationalState.InTransition:
-                        button1.Text = "正在等待";
+                        hotspotButtonConnect.Text = "正在等待";
                         break;
                     case TetheringOperationalState.Unknown:
-                        button1.Text = "未知";
+                        hotspotButtonConnect.Text = "未知";
                         break;
                     default:
                         break;
                 }
             }
-            else if (button1.Text == "关闭热点")
+            else if (hotspotButtonConnect.Text == "关闭热点")
             {
                 switch (tetheringManager.TetheringOperationalState)
                 {
                     case TetheringOperationalState.On:
-                        button1.Text = "正在等待";
+                        hotspotButtonConnect.Text = "正在等待";
                         var result = await tetheringManager.StopTetheringAsync();
                         if (result.Status == TetheringOperationStatus.Success)
-                            button1.Text = "开启热点";
+                            hotspotButtonConnect.Text = "开启热点";
                         break;
                     case TetheringOperationalState.Off:
-                        button1.Text = "开启热点";
+                        hotspotButtonConnect.Text = "开启热点";
                         break;
                     case TetheringOperationalState.InTransition:
-                        button1.Text = "正在等待";
+                        hotspotButtonConnect.Text = "正在等待";
                         break;
                     case TetheringOperationalState.Unknown:
-                        button1.Text = "未知";
+                        hotspotButtonConnect.Text = "未知";
                         break;
                     default:
                         break;
@@ -67,16 +67,16 @@ namespace HotspotManager
                 switch (tetheringManager.TetheringOperationalState)
                 {
                     case TetheringOperationalState.On:
-                        button1.Text = "关闭热点";
+                        hotspotButtonConnect.Text = "关闭热点";
                         break;
                     case TetheringOperationalState.Off:
-                        button1.Text = "开启热点";
+                        hotspotButtonConnect.Text = "开启热点";
                         break;
                     case TetheringOperationalState.InTransition:
-                        button1.Text = "正在等待";
+                        hotspotButtonConnect.Text = "正在等待";
                         break;
                     case TetheringOperationalState.Unknown:
-                        button1.Text = "未知";
+                        hotspotButtonConnect.Text = "未知";
                         break;
                     default:
                         break;
@@ -86,17 +86,17 @@ namespace HotspotManager
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            button1.Text = "开启热点";
-            button1_Click(sender, e);
+            hotspotButtonConnect.Text = "开启热点";
+            hotspotButtonConnect_Click(sender, e);
         }
 
-        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        private void hotspotCheckBoxAutoReconnect_CheckedChanged(object sender, EventArgs e)
         {
-            if (checkBox1.Checked)
+            if (hotspotCheckBoxAutoReconnect.Checked)
             {
                 timer1_Tick(sender, e);
             }
-            timer1.Enabled = checkBox1.Checked;
+            timer1.Enabled = hotspotCheckBoxAutoReconnect.Checked;
         }
 
         private void timer2_Tick(object sender, EventArgs e)
@@ -121,13 +121,13 @@ namespace HotspotManager
             }
         }
 
-        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        private void networkCheckBoxAutoReconnect_CheckedChanged(object sender, EventArgs e)
         {
-            if (checkBox2.Checked)
+            if (networkCheckBoxAutoReconnect.Checked)
             {
                 timer2_Tick(sender, e);
             }
-            timer2.Enabled = checkBox2.Checked;
+            timer2.Enabled = networkCheckBoxAutoReconnect.Checked;
         }
     }
 }
